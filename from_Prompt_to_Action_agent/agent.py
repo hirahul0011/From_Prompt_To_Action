f4c1bef682c0aa7800a7ca9cf6319638dd077858
hirahul0011 <ra_10001@yahoo.co.in>
2026-01-04 00:46:47 +0530

    From_Prompt_To_Action

---
+++ from_Prompt_to_Action_agent/agent.py
@@ -0,0 +1,32 @@
+from google.adk.agents.llm_agent import Agent
+from google.adk.models.google_llm import Gemini
+from google.adk.runners import InMemoryRunner
+from google.adk.tools import google_search
+from google.genai import types
+
+
+
+retry_config=types.HttpRetryOptions(
+    attempts=5,  # Maximum retry attempts
+    exp_base=7,  # Delay multiplier
+    initial_delay=1, # Initial delay before first retry (in seconds)
+    http_status_codes=[429, 500, 503, 504] # Retry on these HTTP errors
+)
+
+# root_agent = Agent(
+#     model='gemini-2.5-flash',
+#     name='root_agent',
+#     description='A helpful assistant for user questions.',
+#     instruction='Answer user questions to the best of your knowledge',
+# )
+
+root_agent = Agent(
+    name="helpful_assistant",
+    model=Gemini(
+        model="gemini-2.5-flash",
+        retry_options=retry_config
+    ),
+    description="A simple agent that can answer general questions.",
+    instruction="You are a helpful assistant. Use Google Search for current info or if unsure.",
+    tools=[google_search],
+)
